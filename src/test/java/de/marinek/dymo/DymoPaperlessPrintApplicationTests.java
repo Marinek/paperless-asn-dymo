9e837dcd71ff99da5aaec0ad02d8bff2b62271f6
Marinek <martin.gasse@gmail.com>
2023-11-01 14:24:39 +0100

    Intial Commit

---
+++ src/test/java/de/marinek/dymo/DymoPaperlessPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package de.marinek.dymo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DymoPaperlessPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
